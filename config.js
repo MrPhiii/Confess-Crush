24cb3ab1e6b886ad4b1c2d8474f34165c2fc7472
MrPhiii <91372188+MrPhiii@users.noreply.github.com>
2021-09-25 18:06:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     titleWeb: "DanIT",
     introTitle: 'Eii bảooo nàyy',
-    introDesc: `Anh không là Binz, trói em bằng cà vạt.
-Anh chỉ là kẻ viết nhạc và trói em bằng tình ca .`,
-    btnIntro: 'Bấm vào đây nhá',
-    title: 'Đây khum phải web tỏ tình đâu, đây là web tỏ tình đó :>',
+    introDesc: `Giờ yêu ai cũng zậy thôi yêu đỡ tui nàyy`,
+    btnIntro: 'Bấm vào đây nháaa',
+    title: '',
     desc: 'Giả bộ nói yêu tui được khumm?',
     btnYes: 'Ừm thì cũng chịuuu',
     btnNo: 'Khummm :((',
     question: 'Tại sao lại muốn yêu tui zạaa???',
-    btnReply: 'Chịu thì chịu khum chịu cũng phải chịuu',
+    btnReply: 'Đây nèee',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33',
-    mess: 'Hihii',
-    messDesc: 'Khung qua trời khungggggg',
-    btnAccept: 'Dean mới bấm vào đâyy',
+    mess: 'À zậy đóo',
+    messDesc: 'Làm liền đi nháa',
+    btnAccept: 'Bái baiiii',
     messLink: 'https://m.me/MrPhi0608' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
